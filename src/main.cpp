82c7b982c95756e8faa336cec6fdc8b2592a4585
pfuentes69 <pfuentes69@naviter.es>
2020-01-20 21:38:17 +0100

    DeepSleep OK

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 DHTesp dht;
 
 IPAddress server(192,168,2,101);
-IPAddress ip(192, 168, 2, 25); 
+IPAddress ip(192, 168, 2, 26); 
 IPAddress gateway(192, 168, 2, 1);
 IPAddress subnet(255, 255, 255, 0);
 IPAddress dns(8, 8, 8, 8); 
@@ -25,37 +25,16 @@ WiFiClient WIFIclient;
 PubSubClient client(WIFIclient);
 
 // sleep for this many seconds
-const int sleepSeconds = 5;
-
-void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Attempt to connect, just a name to identify the client
-    if (client.connect("DHTSensor2")) {
-      Serial.println("connected");
-      // Once connected, publish an announcement...
-      client.publish("Papillon/dev02/status","hello world");
-      // ... and resubscribe
-//      client.subscribe("presence");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
-}
+const int sleepSeconds = 1800;
 
 void setup() {
   // initialize serial for debugging
   Serial.begin(115200);
   Serial.println();
-  Serial.println("******* SETUP START *******");
+//  Serial.println("******* SETUP START *******");
 
   // Connect D0 to RST to wake up
-  //pinMode(0, WAKEUP_PULLUP);
+  pinMode(0, WAKEUP_PULLUP);
   // initialize WiFi
   WiFi.mode(WIFI_STA);
   WiFi.config(ip, gateway, subnet, dns);
@@ -65,10 +44,10 @@ void setup() {
   while (connectingWIFI) {
     WiFi.begin(ssid, password);
     tries = 0;    
-    Serial.println("Trying Main WiFi");
+//    Serial.println("Trying Main WiFi");
     while ((WiFi.status() != WL_CONNECTED) && (tries < 10)) {
       delay(500);
-      Serial.print(".");
+//      Serial.print(".");
       tries++;
     }
     Serial.println();
@@ -80,37 +59,40 @@ void setup() {
     }
   }
 
-  Serial.println("");
-  Serial.println("WiFi connected");  
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+//  Serial.println("");
+//  Serial.println("WiFi connected");  
+//  Serial.println("IP address: ");
+//  Serial.println(WiFi.localIP());
 
   //connect to MQTT server
   client.setServer(server, 1883);
 
   dht.setup(DHTPIN, DHTesp::DHT11);
-  delay(1000); // Stabilize Sensors
+  delay(500); // Stabilize Sensors
 
-  // put your main code here, to run repeatedly:
-  if (!client.connected()) {
-    reconnect();
-  }
+//  if (!client.connected()) {
+//    reconnect();
+//  }
 
+  client.connect("DHTSensor2");
+//  Serial.println("connected");
+  // Once connected, publish an announcement...
+//  client.publish("PapillonIoT/DHTSensor2/status","UP");
   float humidity = dht.getHumidity() * 1.3;
   if (humidity > 100) {
     humidity = 100;
   } 
   float temperature = dht.getTemperature() * 0.9;
 
-  String payload = "{\"temperature\":";
+  String payload = "{\"Temperature\":";
   payload += temperature;
-  payload += ",\"humidity\":";
+  payload += ",\"Humidity\":";
   payload += humidity;
   payload += "}";
 
-  Serial.println(payload);
+  //Serial.println(payload);
   client.publish("PapillonIoT/DHTSensor2/data", (char*) payload.c_str());
-
+  delay(500);
   // Let's go to sleep
   ESP.deepSleep(sleepSeconds * 1000000);
 
